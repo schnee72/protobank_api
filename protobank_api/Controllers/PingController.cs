00fa0511feffee93c20bff0df7ce42ff6d91db5f
Dustin Schnee <schnee72@gmail.com>
2018-08-05 20:12:19 -0500

    initial

---
+++ protobank_api/Controllers/PingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace protobank_api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Ping")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "pong";
+        }
+
+        [HttpGet("two")]
+        public string GetTwo()
+        {
+            return "pong2";
+        }
+    }
+}
